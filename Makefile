612b65e5ab9ce9d229e43492783a76e3edeb404e
alifoo <alissonayres.trb@gmail.com>
2025-09-28 17:41:04 -0300

    add glew lib

--- Makefile
+++ Makefile
@@ -1,19 +1,22 @@
 # CXX = C++ compiler variable in makefiles.
 # -Wall sets on all common compiler warnings, and -Wextra turns on extra warnings.
 # -I Tells the compiler where to look for header files.
+#  -DGLEW_STATIC is used to tell the glew header files that we intend to link against the static version of the lib.
 CXX = clang++
-CXXFLAGS = -std=c++17 -Wall -Wextra -I/opt/homebrew/Cellar/glfw/3.4/include
+CXXFLAGS = -std=c++17 -Wall -Wextra -DGLEW_STATIC -I/opt/homebrew/Cellar/glfw/3.4/include -I/opt/homebrew/Cellar/glew/2.2.0_1/include
 
 # Source and object files
-SRC = $(wildcard *.cpp)
+SRC = $(wildcard src/*.cpp)
 OBJ = $(SRC:.cpp=.o)
 
 # Output binary
 OUT = app
 
-# Libraries & frameworks (macOS + GLFW)
+# Libraries & frameworks (macOS + GLFW + Glew)
 LIBS = -L/opt/homebrew/Cellar/glfw/3.4/lib \
        -lglfw \
+       -L/opt/homebrew/Cellar/glew/2.2.0_1/lib \
+       -lglew \
        -framework OpenGL \
        -framework Cocoa \
        -framework IOKit \
--- src/Application.cpp
+++ src/Application.cpp
@@ -1,40 +1,45 @@
+#include "/opt/homebrew/Cellar/glew/2.2.0_1/include/GL/glew.h"
 #include "/opt/homebrew/Cellar/glfw/3.4/include/GLFW/glfw3.h"
+#include <iostream>
 
 int main(void) {
-  GLFWwindow *window;
+    GLFWwindow *window;
 
-  /* Initialize the library */
-  if (!glfwInit())
-    return -1;
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
 
-  /* Create a windowed mode window and its OpenGL context */
-  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-  if (!window) {
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error!" << std::endl;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
     glfwTerminate();
-    return -1;
-  }
-
-  /* Make the window's context current */
-  glfwMakeContextCurrent(window);
-
-  /* Loop until the user closes the window */
-  while (!glfwWindowShouldClose(window)) {
-    /* Render here */
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glBegin(GL_TRIANGLES);
-    glVertex2f(-0.5f, -0.5f);
-    glVertex2f(0.0f, 0.5f);
-    glVertex2f(0.5f, -0.5f);
-    glEnd();
-
-    /* Swap front and back buffers */
-    glfwSwapBuffers(window);
-
-    /* Poll for and process events */
-    glfwPollEvents();
-  }
-
-  glfwTerminate();
-  return 0;
+    return 0;
 }
